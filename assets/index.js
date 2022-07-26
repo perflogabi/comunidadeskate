e9731ea5db0673958144e07a724b352227025742
Gabriela Florêncio <perflogabi@gmail.com>
2022-07-25 21:56:40 -0300

    primeiro commit

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,5 @@
 const botaoMenu = document.querySelector('.cabecalho__menu')
-const menu = document.querySelector('menu-lateral')
+const menu = document.querySelector('.menu-lateral')
 
 botaoMenu.addEventListener('click', () => {
     menu.classList.toggle('menu-lateral--ativo')
